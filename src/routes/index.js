c00a2cd7a4703f20fc7b9c92c749d80d2b3929b5
HELL0ANTHONY <george.anthony100@hotmail.com>
2021-08-12 11:44:39 -0300

    refactor: Error handle

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const routerHelper = require("../helpers/routerHelper");
+const getTeam = require("../controllers/getTeams");
 const getPlayers = require("../controllers/getPlayers");
+
 const router = Router();
 
-router.post("/api/v1/team", routerHelper(getPlayers));
+router.post("/api/v1/team", routerHelper(getTeam));
+router.get("/api/v1/players", routerHelper(getPlayers));
 
 module.exports = router;
